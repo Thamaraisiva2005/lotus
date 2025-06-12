f1ca57b016961a64dc53df8a0ed76e90858e65e8
Divansan <divansan05@gmail.com>
2025-06-12 15:23:18 +0530

    Configure .gitignore, package.json, vite.config.js and src/

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/lotusport/', // 👈 use your GitHub repo name
+  base: '/lotus/', // 👈 use your GitHub repo name
   plugins: [react()],
 })
