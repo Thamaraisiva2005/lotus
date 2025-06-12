fa6b31ce9d42eff9c082e6f66f368ecfb2b1bb04
Divansan <divansan05@gmail.com>
2025-06-12 16:08:55 +0530

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/lotus/', // 👈 use your GitHub repo name
+  base: '/lo/', // 👈 use your GitHub repo name
   plugins: [react()],
 })
